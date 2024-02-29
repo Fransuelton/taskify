d7a0fd9132381da607fed412ce5fcdaed7f06e37
Fransuelton <elton6103@gmail.com>
2024-02-29 19:58:34 -0300

    feat: Add button and link to register page in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,13 @@
-
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-
   return (
-    <section className="flex justify-center items-center h-screen gap-28 bg-slate-900 ">
+    <section className="flex justify-center flex-col items-center h-screen gap-28 bg-slate-900 ">
       <h1 className="text-white text-xl">Home</h1>
+      <Link to={"/register"}>
+      <Button>Registrar</Button>
+      </Link>
     </section>
   );
 };
