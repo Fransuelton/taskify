3ee443b39e2a26606e54056301fd781eadce4d5b
Fransuelton <elton6103@gmail.com>
2024-02-29 17:50:15 -0300

    feat: Add Home route to routes.tsx

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Login } from "./Login";
 import { Register } from "./Register";
+import { Home } from "./Home";
 
 const AppRoutes = () => (
   <BrowserRouter>
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
+
     </Routes>
   </BrowserRouter>
 );
