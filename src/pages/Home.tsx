18cfd3caacd92e9fc6f6b01dfb7ccfe414b6e1eb
Fransuelton <elton6103@gmail.com>
2024-02-29 20:38:49 -0300

    refactor: refactor layout

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,12 +3,22 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <section className="flex justify-center flex-col items-center h-screen gap-28 bg-slate-900 ">
-      <h1 className="text-white text-xl">Home</h1>
-      <Link to={"/register"}>
-      <Button>Registrar</Button>
-      </Link>
-    </section>
+    <>
+      <section className="flex justify-center flex-col items-center h-screen gap-28 bg-slate-900">
+        <h1 className="text-4xl text-white">
+          Bem-vindo(a) ao <span className="text-orange-500">Taskify</span>
+        </h1>
+        <h2 className="text-white text-xl">
+          Sua solução para manter suas tarefas organizadas de forma simples e
+          eficiente. Comece a aumentar sua produtividade hoje mesmo!
+        </h2>
+        <Link to={"/register"}>
+          <Button>Começar agora mesmo!</Button>
+        </Link>
+        <div>
+        </div>
+      </section>
+    </>
   );
 };
 
