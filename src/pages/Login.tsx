befaa30db14d0afc50c0631491adbdbe8b6ce636
Fransuelton <elton6103@gmail.com>
2024-02-29 17:07:44 -0300

    feat: Add email and password input fields and implement login functionality

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -2,8 +2,29 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import loginImage from "/undraw_login.svg";
 import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useState } from "react";
+import { auth } from "@/services/firebaseConfig";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [signInWithEmailAndPassword, user, loading, error] =
+    useSignInWithEmailAndPassword(auth);
+
+  function handleSignIn() {
+    signInWithEmailAndPassword(email, password);
+  }
+
+  if (user) {
+    console.log("Usuário Logado: ", user);
+  }
+
+  if (error) {
+    console.log(error);
+  }
+
   return (
     <section className="flex justify-center items-center h-screen gap-28 bg-slate-900 ">
       <img src={loginImage} alt="login image" className="w-96 h-96" />
@@ -12,15 +33,25 @@ const Login = () => {
         <p className="text-base">
           Faça login abaixo para acessar sua lista de tarefas.
         </p>
-        <Input placeholder="Email" />
-        <Input placeholder="Senha" />
+        <form className="flex flex-col gap-3">
+          <Input
+            placeholder="Email"
+            type="email"
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <Input
+            placeholder="Senha"
+            type="password"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </form>
         <p>
           Não tem uma conta?{" "}
           <Link to={"/register"} className="text-orange-500">
             Clique aqui
           </Link>
         </p>
-        <Button>Login</Button>
+        <Button onClick={handleSignIn}>Login</Button>
       </div>
     </section>
   );
