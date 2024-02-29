b8e13ae34ab5f22bf2c3a70a9781796eb4542aed
Fransuelton <elton6103@gmail.com>
2024-02-29 17:49:59 -0300

    feat: Add Home component

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+
+
+const Home = () => {
+
+  return (
+    <section className="flex justify-center items-center h-screen gap-28 bg-slate-900 ">
+      <h1 className="text-white text-xl">Home</h1>
+    </section>
+  );
+};
+
+export { Home };
